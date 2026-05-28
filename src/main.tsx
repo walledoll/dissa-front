4050c8c20d8783697e8190d2a7cabe9ed8d3733b
VDanil <DValiahmetov@IBS.ru>
2026-05-28 09:36:58 +0300

    Feature | Поддержка основных шрифтов

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './app/styles/index.scss'
-import App from './app/App.tsx'
-import { BrowserRouter } from 'react-router'
+import { StrictMode } from "react";
+import { BrowserRouter } from "react-router";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./app/App.tsx";
+import "./app/styles/index.scss";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
